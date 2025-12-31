738a26269a1f4db293e9792d8d3adecd155a699d
xerdin442 <mudianthonio27@gmail.com>
2025-12-31 12:02:49 +0100

    fixed vercel build error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  transpilePackages: ['@coinbase/cdp-sdk', '@reown/appkit'],
   images: {
     remotePatterns: [
       {
